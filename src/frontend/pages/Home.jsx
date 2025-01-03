25ccf19bd8212695b460d23eb2b7cb168b67fbb1
mahadhsn <mahadhassan375@gmail.com>
2025-01-02 23:41:27 -0500

    Fix styling

--- src/frontend/pages/Home.jsx
+++ src/frontend/pages/Home.jsx
@@ -48,20 +48,21 @@ const Home = () => {
           Honours <a href='https://www.eng.mcmaster.ca/cas/degree-options/software-engineering/' target="_blank" rel="noopener noreferrer" className='underline'>Software Engineering</a> student 
           at <a href='https://www.mcmaster.ca' target="_blank" rel="noopener noreferrer" className='underline'>McMaster University</a> 🦅 
           based in 
-          Hamilton, Ontario 🍁. Outside the coding world, I'm probably singing (checkout my TikTok!), learning a new skill, 
-          or <span ><ChangeWord list={hobbies}/></span>.
+          Hamilton, Ontario 🍁. Outside the coding world, I'm probably 
+          singing <span className='font-light'>(checkout my TikTok!)</span>, learning a new skill, 
+          or <span><ChangeWord list={hobbies}/></span>.
         </p>
 
         <p className='mb-6'>
           At the heart of it all, I believe in constant self-improvement. As <span className='font-light'>Boonaa Mohammed</span> once 
           said, <span className='italic font-semibold'> "If the whole world was blind, how many people you impress?"</span>. This
           quote has always helped me focus growth and impact over vanity and 
-          ego. <span className='text-m md:text-lg'>(check out more below!)</span>
+          ego. <span className='font-light'>(check out more below!)</span>
         </p>
 
         <p className=''>
-          I’d love to connect with you! Feel free to reach out through my contact page or drop me an email at 
-          <a href='mailto:mahadhassan.hello@gmail.com' target="_blank" rel="noopener noreferrer" className='underline'>mahadhassan.hello@gmail.com</a>
+          I’d love to connect with you! Feel free to reach out through my contact page or drop me an email 
+          at <a href='mailto:mahadhassan.hello@gmail.com' target="_blank" rel="noopener noreferrer" className='underline'>mahadhassan.hello@gmail.com</a>
             . I'm always open to new opportunities and collaborations! 🚀
         </p>
         </span>
