2ac68bd2a5582df70b381b7b499eb1950a432c48
mahadhsn <mahadhassan375@gmail.com>
2025-01-06 18:25:28 -0500

    feat: add copyright & last update date in footer

--- src/frontend/components/Footer.jsx
+++ src/frontend/components/Footer.jsx
@@ -3,47 +3,55 @@ import { FaLinkedin, FaGithub, FaEnvelope, FaInstagram, FaTiktok } from 'react-i
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center space-x-6 py-4 text-white">
+    <footer className="">
       
-      <a 
-        href="mailto:mahadhassan.hello@gmail.com" 
-        target="_blank" 
-        rel="noopener noreferrer" 
-        className="hover:text-cyan-500 transition-all duration-300">
-        <FaEnvelope size={24} />
-      </a>
+      <div className='flex justify-center items-center space-x-6 py-3 text-white'>
+        <a 
+          href="mailto:mahadhassan.hello@gmail.com" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="hover:text-cyan-500 transition-all duration-300">
+          <FaEnvelope size={24} />
+        </a>
 
-      <a 
-        href="https://www.linkedin.com/in/mahad-hassan/" 
-        target="_blank" 
-        rel="noopener noreferrer" 
-        className="hover:text-cyan-500 transition-all duration-300">
-        <FaLinkedin size={24} />
-      </a>
+        <a 
+          href="https://www.linkedin.com/in/mahad-hassan/" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="hover:text-cyan-500 transition-all duration-300">
+          <FaLinkedin size={24} />
+        </a>
 
-      <a 
-        href="https://github.com/mahadhsn" 
-        target="_blank" 
-        rel="noopener noreferrer" 
-        className="hover:text-cyan-500 transition-all duration-300">
-        <FaGithub size={24} />
-      </a>
+        <a 
+          href="https://github.com/mahadhsn" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="hover:text-cyan-500 transition-all duration-300">
+          <FaGithub size={24} />
+        </a>
 
-      <a 
-        href="https://www.instagram.com/mahadhssn/" 
-        target="_blank" 
-        rel="noopener noreferrer" 
-        className="hover:text-cyan-500 transition-all duration-300">
-        <FaInstagram size={24} />
-      </a>
+        <a 
+          href="https://www.instagram.com/mahadhssn/" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="hover:text-cyan-500 transition-all duration-300">
+          <FaInstagram size={24} />
+        </a>
 
-      <a 
-        href="https://www.tiktok.com/@aguitarer" 
-        target="_blank" 
-        rel="noopener noreferrer" 
-        className="hover:text-cyan-500 transition-all duration-300">
-        <FaTiktok size={24} />
-      </a>
+        <a 
+          href="https://www.tiktok.com/@aguitarer" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="hover:text-cyan-500 transition-all duration-300">
+          <FaTiktok size={24} />
+        </a>
+      </div>
+
+      <div className='text-center text-white py-4'>
+        <p></p>
+        <p>mahadhssn &copy; 2025 | Updated: 6/1/2025</p>
+      </div>
+
     </footer>
   );
 }
