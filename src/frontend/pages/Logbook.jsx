dbded56b769bab408f18f95857be46960d40a275
mahadhsn <hassam87@mcmaster.ca>
2025-05-21 12:22:35 -0400

    basic logbook setup

--- src/frontend/pages/Logbook.jsx
+++ src/frontend/pages/Logbook.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom';
 
 const Logbook = () => {
+
+  useEffect(() => {
+    document.title = 'Mahad\'s Life'
+  },[])
+
   return (
-    <div>
-      
+    <div className='text-white flex flex-col opacity-0 animate-fade-in mt-[5%] md:mt-[18%] items-center'>
+
+      <h1 className='text-3xl md:text-5xl text-center'>
+        Logbook!
+      </h1>
+
+
+
     </div>
   )
 }
