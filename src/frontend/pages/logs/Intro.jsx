8a00389ec499a51d8540dfcc3e523a8a46dc86db
mahadhsn <hassam87@mcmaster.ca>
2025-06-20 22:56:09 -0400

    Intro - plans completed

--- src/frontend/pages/logs/Intro.jsx
+++ src/frontend/pages/logs/Intro.jsx
@@ -6,7 +6,7 @@ const Intro = () => {
         document.title = "Intro Log"
     } )
   return ( 
-    <div className='min-h-screen text-textlight dark:text-textdark flex flex-col opacity-0 animate-fade-in mt-[5%] md:mt-[18%] w-full'>
+    <div className='text-textlight dark:text-textdark animate-fade-in mt-[5%] md:mt-[18%] w-full ml-5'>
 
       <h1 className='text-3xl font-bold text-center items-center justify-center align-middle mb-2'>
         Intro Log :) 
@@ -27,7 +27,7 @@ const Intro = () => {
       <ol className='list-disc list-outside mb-4 text-lg space-y-2'>
         <li>
           <span className='font-bold'>NO AI!</span> — We often blindly use AI in everything nowadays and I feel it makes it difficult for us to be authentic.
-          You'll be seeing me myself and I on this blob of the internet.
+          You'll be seeing me myself and I on this blob of the internet. The max I'll do is use it for motivation maybe... pls dont end me
         </li>
         <li>
           <span className='font-bold'>NO FILTER!</span> - I'm going to attempt to be as transparent as possible about my views, opinions, and experiences.
@@ -38,27 +38,63 @@ const Intro = () => {
         </p>
       </ol>
 
-      <h1 className='text-xl font-bold mb-1'>Why?</h1>
+      <div className='mb-6'>
+        <h1 className='text-xl font-bold mb-1'>Why?</h1>
+        <p className='text-lg mb-1'>
+          Frankly, idk. If you think about it, this page is in the depths of the internet on a page not a lot of people would visit.
+          That gives it a nice bit of balance from being both too public or too private. Maybe its a sort of public diary which gives me a wierd sense of comfort?
+        </p>
+        <p className='text-lg mb-1'>
+          I'm also at a point in life where I want to try out new things cause why not, you know? What's the worst that could happen?
+          Maybe my future employer will see this and think "lmao what is this guy doing?" and throw my resume in the trash.
+          BUT, they could also think "wow, he took a risk, dude seems interesting." and still throw my resume in the trash. On the other hand, maybe someone I employ
+          will see this and think "wow, I'd love to work with this guy". There's a million things that COULD happen. Why not focus on what's in your hand?
+        </p>
+        <p className='text-lg mb-1'>
+          At the end of the day,
+          I keep itching towards the idea of not caring about the opinions of others. Not in a self-centered or rude way, but in more so in a "it's my life and I think
+          I should do what I want" way. Hey, I can make a whole logbook page out of that!
+        </p>
+        <p className='text-lg mb-1'>
+          Moreso, I might affect someone in a positive way. Maybe someone will read this and think "damn, he's speaking facts" and NOT throw my resume in the trash.
+          LOL just kidding not everything's about getting a job. Or is it... Anyway, I'd like to think I can help someone out there in a way too. To me, there are almost 
+          no negatives to this. Either I help someone out, I learn how to finally write better, or no one ever reads this and I just have a nice place to vent my thoughts. 
+        </p>
+      </div>
+
+      <div className='mb-4'>
+        <h1 className='text-xl font-bold mb-1'>Plans?</h1>
+        <p className='text-lg mb-4'>
+          At this point, I wanna write about a lot but you know how that usually goes.
+          I'll try to hold myself accountable and meet those standards but you never know.
+          My current plans are to write about 3 mains categories which you might've noticed on the main logbook page:
+        </p>
+        <ol className='list-disc list-outside mb-4 text-lg space-y-2'>
+          <li>
+            <span className='font-bold'>life</span> — Here I want to write about my personal life choices.
+            This would include anything from hobbies, interests, personal growth, etc...
+          </li>
+          <li>
+            <span className='font-bold'>school</span> — This will focus on my academic life.
+            I'd like to think I'll keep it going throughout my life incase I pursue a master's or whatnot.
+          </li>
+          <li>
+            <span className='font-bold'>career</span> — I think this would be interchangeable with school at times 
+            but would focus more on professional development and stuff.
+          </li>
+        </ol>
+      </div>
+
       <p className='text-lg mb-1'>
-        Frankly, idk. If you think about it, this page is in the depths of the internet on a page not a lot of people would visit.
-        That gives it a nice bit of balance of being too public or too private. Maybe its a sort of public diary which gives me a wierd sense of comfort?
+        Other than these, I might add reviews on courses I've taken at Mac which I guess would come under school.
+        I could also include some of th external exams I've done such as the SAT, APs, IELTS, etc...
       </p>
       <p className='text-lg mb-1'>
-        I'm also at a point in life where I want to try out new things cause why not you know? What's the worst that could happen?
-        Maybe my future employer will see this and think "lmao what is this guy doing?" and throw my resume in the trash.
-        BUT, they could also think "wow, he took a risk, dude seems interesting." and still throw my resume in the trash. On the other hand, maybe someone I employ
-        will see this and think "wow, I'd love to work with this guy". There's a million things that COULD happen. Why not focus on what's in your hand?
+        I thought of also adding any notes I made for courses which might help out someone out there.
+        I know how much I struggled at times with those so if it can help someone, I'll be glad.
       </p>
-      <p className='text-lg mb-1'>
-        At the end of the day,
-        I keep itching towards the idea of not caring about the opinions of others. Not in a self-centered or rude way, but in more so in a "it's my life and I think
-        I should do what I want" way. Hey, I can make a whole logbook page out of that! Added it to my list, hopefully it sees the light of day at some point.
-      </p>
-      <p className='text-lg mb-1'>
-        Moreso, I might affect someone in a positive way. Maybe someone will read this and think "damn, he's speaking facts" and NOT throw my resume in the trash.
-        LOL just kidding not everything's about getting a job. Or is it... Anyway, I'd like to think I can help someone out there in a way too. To me, there are almost 
-        no negatives to this. Either I help someone out, I learn how to finally write better, or no one ever reads this and I just have a nice place to vent my thoughts.
-      </p>
+
+      
 
     </div>
   )
