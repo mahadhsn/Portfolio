dbf47ec9875cededf4559156c6fedb8033c7dfd7
mahadhsn <hassam87@mcmaster.ca>
2025-08-19 20:47:43 -0400

    feature: posthog init

--- src/frontend/main.jsx
+++ src/frontend/main.jsx
@@ -3,11 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext";
 import App from "./App.jsx";
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-05-24",
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </PostHogProvider>
   </StrictMode>,
 );
